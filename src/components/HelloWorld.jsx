0dc50da42dd99983cdfec443e2267966fd799545
Alfredo Aguirre <thefurok@gmail.com>
2020-10-17 16:58:51 -0500

    Añadimos el header como primer componente a la aplicacion

--- src/components/HelloWorld.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';
-
-const HelloWorld = () => <h1>Ya no quiero programar :'c </h1>;
-export default HelloWorld;
---
+++ src/containers/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/HeaderVideo';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
