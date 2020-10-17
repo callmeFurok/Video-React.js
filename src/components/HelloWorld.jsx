cd27c5cc7309203d250742a55b83517b43c38d11
Alfredo Aguirre <thefurok@gmail.com>
2020-10-17 16:12:40 -0500

    soy humanista pero diosito santo estas configuraciones me mataron, pero lo logre con sangre sudor y lagrimas

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => <h1>Ya no quiero programar :'c </h1>;
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Este archivo es la entrada de nuestra app
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
