45bf7c890e01086191b01bc0e6b889d7fae7bdd4
Alfredo Aguirre <thefurok@gmail.com>
2020-10-17 23:42:13 -0500

    Añadimoslos estilos y el componente del buscador

--- src/components/HeaderVideo.jsx
+++ src/components/HeaderVideo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../assets/styles/components/Header.scss';
 const Header = () => (
   <header className="header">
     <img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video" />
